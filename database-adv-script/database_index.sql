2c20b03a32d5e290e743adce1f397a033b7ed620
sofy <bedlusofi@gmail.com>
2024-12-02 14:50:01 +0300

    Add indexes for user and booking tables and include EXPLAIN ANALYZE for runtime metrics

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,4 +1,11 @@
+---Creating indexes for the database tables:
 CREATE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_property_host_id ON Property(host_id);
 CREATE INDEX idx_booking_property_id ON Booking(property_id);
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
+
+
+---Using EXPLAIN ANALYZE for detailed runtime metrics:
+EXPLAIN ANALYZE SELECT user_id, COUNT(*) AS total_bookings
+FROM Booking
+GROUP BY user_id;
